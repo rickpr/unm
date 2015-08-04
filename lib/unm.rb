25e4c4b343a09793d43ad5e955f7c30dcd1d30fb
Ricardo Piro-Rael <fdisk@fdisk.co>
2015-08-04 15:25:38 -0600

    Moved to production catalog

--- lib/unm.rb
+++ lib/unm.rb
@@ -47,7 +47,7 @@ module Unm
 
     def self.get
       return @catalog if @catalog
-      subjects = HTTParty.get("http://catalog-devl.unm.edu/catalogs/2015-2016/subjects-and-courses.xml")["data"]["subjects"]["subject"]
+      subjects = HTTParty.get("http://catalog.unm.edu/catalogs/2015-2016/subjects-and-courses.xml")["data"]["subjects"]["subject"]
       @catalog = subjects.map do |subject|
         courses = subject["course"]
         name    = subject["subjectName"]
@@ -56,7 +56,7 @@ module Unm
           course_page = Nokogiri::HTML.parse(HTTParty.get(URI.escape course["path"])) rescue course["path"]
           description = course_page.css('.content > p').first.text rescue "Error loading description. Check #{course["path"]}." 
           prerequisites = find_prerequisites(course_page) rescue "Error loading prerequisites. Check #{course["path"]}."
-          hours = course_page.css('b').text.match(/\(\D*(\d).*\)/)[1].to_i rescue binding.pry#"Error loading hours. Check #{course["path"]}."
+          hours = course_page.css('b').text.match(/\(\D*(\d).*\)/)[1].to_i rescue "Error loading hours. Check #{course["path"]}."
           { "name" => course_name, "description" => description, "prerequisites" => prerequisites, "hours" => hours }
          end }
       end
